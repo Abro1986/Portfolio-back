ea67268472fe184959943c1345a3bb6ae95d7cd5
Abro1986 <AndrewBroestl@gmail.com>
2018-09-12 09:57:34 -0600

    simple commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flywater-back
---
+++ server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+let bodyParser = require('body-parser')
+let express    = require('express');
+let app        = express();
+let cors       = require('cors');
+let router     = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({
+	extended: false
+}));
+
+
+
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+
+app.post('/api/mail', function(req,res) {
+
+
+	console.log(req.body)
+	let msgTwo = {
+  to: 'AndrewBroestl@gmail.com',
+  from: req.body.email,
+  subject: req.body.subject,
+  text: req.body.text,
+  html: `<strong>${req.body.text}</strong>`,
+};
+
+	sgMail.send(msgTwo);
+//	res.sendStatus(200)
+	res.send('Thank You')
+})
+
+app.get('/api/all', function(req, res) {
+	res.send('hello')
+})
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log(`listening on port ${ port }`);
+});
